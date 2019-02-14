c7f03e1625b0ed46a422e7b7561ffcecfc8c887d
Manuel Savini <manuel.savini@local.ittcesena.it>
2019-02-14 09:48:47 +0100

    .

--- index.php
+++ index.php
@@ -24,12 +24,12 @@
 			$result = query1($reparto, $id_libro);
 
 			deliver_response(200,"Quantità", $result);
-
+			break;
 			case 2:
 			
 			$sconti = get_sconti();
 			deliver_response(200,"Elenco sconto libri", $sconti);
-			
+			break;
 			case 3:
 			
 			d1 = $_GET['d1'];
@@ -37,12 +37,13 @@
 			
 			libri_date = get_date(d1, d2);
 			deliver_response(200,"Libri all' interno del range", $lbri_date);
-			
+			break;
 			case 4:
 			
 			
-			
+			break;
 			case default: echo("Il numero della query non esiste");
+			break;
 				}
 	else
 	{
